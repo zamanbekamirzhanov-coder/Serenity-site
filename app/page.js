288e41dc754883f4cb012009a8657a7827e885c4
zamanbekamirzhanov-coder <zamanbek.amirzhanov@gmail.com>
2026-06-18 01:54:53 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -809,8 +809,14 @@ export default function SerenityLanding() {
                 <a href={`tel:+${WHATSAPP_NUMBER}`} className="sr-footer-link" style={{ fontWeight: 500 }}>
                   +7 776 926 28 88
                 </a>
-              </div>
+              </div> </div> <div>
+              <h4>Услуги</h4>
+              <a href="/himchistka-kurtok" className="sr-footer-link">Химчистка курток</a>
+              <a href="/himchistka-obuvi" className="sr-footer-link">Химчистка обуви</a>
+              <a href="/chistka-shtor" className="sr-footer-link">Чистка штор</a>
+              <a href="/stirka-dlya-biznesa" className="sr-footer-link">Для бизнеса</a>
             </div>
+            </div></div>
           </div>
           <div className="sr-footer-bottom">
             © {new Date().getFullYear()} Serenity. {t.footerRights}
